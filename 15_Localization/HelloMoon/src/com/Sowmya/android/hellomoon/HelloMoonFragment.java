2f42348414881fdda00ce9c8fab62133b0a8693a
Sowmya Hariharan <scarlet.m7@gmail.com>
2013-10-08 16:44:06 -0400

    Adds localization support. Supports Spanish text and audio files
    .

---
+++ 15_Localization/HelloMoon/src/com/Sowmya/android/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,50 @@
+package com.Sowmya.android.hellomoon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HelloMoonFragment extends Fragment {
+	private Button mPlayButton;
+	private Button mStopButton;
+	private AudioPlayer mPlayer = new AudioPlayer();
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflator, ViewGroup parent, Bundle savedInstanceState){
+		View v = inflator.inflate(R.layout.fragment_hello_moon, null);
+		mPlayButton = (Button) v.findViewById(R.id.hellomoon_playButton);
+		mPlayButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mPlayer.play(getActivity());
+			}
+		});
+		
+		mStopButton = (Button) v.findViewById(R.id.hellomoon_stopButton);
+		mStopButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mPlayer.stop();
+			}
+		});
+		return v;
+	}
+	
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		mPlayer.stop();
+	}
+}
